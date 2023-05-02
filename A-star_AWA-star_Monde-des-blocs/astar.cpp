ac2234f92fef503a2d600352fcbdba7592552400
Milo Fournier <132404970+MiloFournier@users.noreply.github.com>
2023-05-03 01:00:00 +0200

    Add files via upload

--- A-star_AWA-star_Monde-des-blocs/astar.cpp
+++ A-star_AWA-star_Monde-des-blocs/astar.cpp
@@ -1,9 +1,3 @@
-/*
- Implementation of A* for solving planning problems
- Copyright (C) 2023 Christine Solnon
- Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier au titre des clauses de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation. Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même une garantie implicite de COMMERCIABILITE ou DE CONFORMITE A UNE UTILISATION PARTICULIERE. Voir la Licence Publique Générale GNU pour plus de détails.
- */
-
 #include <queue>
 #include <unordered_map>
 #include <limits>
