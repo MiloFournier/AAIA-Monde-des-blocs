29f2741bf057eb1c563e06640ebe037b8cf5eddb
Milo Fournier <132404970+MiloFournier@users.noreply.github.com>
2023-05-03 01:10:19 +0200

    Update state.cpp

--- A-star_AWA-star_Monde-des-blocs/state.cpp
+++ A-star_AWA-star_Monde-des-blocs/state.cpp
@@ -1,9 +1,3 @@
-/*
- Implementation of class State for the bloc world problem
- Copyright (C) 2023 Christine Solnon
- Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier au titre des clauses de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation. Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même une garantie implicite de COMMERCIABILITE ou DE CONFORMITE A UNE UTILISATION PARTICULIERE. Voir la Licence Publique Générale GNU pour plus de détails.
- */
-
 #include <iostream>
 
 class State{
